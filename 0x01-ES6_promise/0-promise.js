07ccbf6f479de2daf68cc2843ef7ceba91e0c63d
Layla Abkari <lolabkari@gmail.com>
2024-04-11 05:30:34 +0000

    Es6 Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,7 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {});
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+        // Placeholder function with empty Promise constructor
+        // This function doesn't perform any asynchronous operation
+        // It's intended as a placeholder for further implementation
+    });
 }
-
-export default getResponseFromAPI();
