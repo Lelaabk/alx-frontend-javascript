7af12c9908b368bb4c168a6f128ae02823dc01fa
Layla Abkari <lolabkari@gmail.com>
2024-04-11 05:22:37 +0000

    Es6 Promise

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+    const u1 = uploadPhoto();
+    const u2 = createUser();
+
+    return  Promise.all([u1, u2]).then((value) => {
+        console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+    }).catch(() => { console.log('Signup system offline')});
+}
