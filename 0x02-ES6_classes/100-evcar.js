e397f702377da6fd26be33b3178c6c94174c3984
Layla Abkari <lolabkari@gmail.com>
2024-04-12 03:08:25 +0000

    Classes

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,14 +1,12 @@
-// File: 100-evcar.js
-import Car from './10-car.js';
+import Car from './10-car';
 
 export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    this._range = range;
-  }
+    constructor(brand, motor, color, range) {
+        super(brand, motor, color);
+        this._range = range;
+    }
 
-  cloneCar() {
-    const { _brand, _motor, _color } = this;
-    return new Car(_brand, _motor, _color);
-  }
+    cloneCar() {
+        return new Car();
+    }
 }
