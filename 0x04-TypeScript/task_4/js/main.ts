e3b23c68406919bc0b65fab8c34700a067575a37
Layla Abkari <lolabkari@gmail.com>
2024-04-18 00:57:29 +0100

    Namespace & Declaration merging

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,34 @@
+// main.ts
+
+import { Subjects } from './subjects/Subject';
+import { Cpp } from './subjects/Cpp';
+import { Java } from './subjects/Java';
+import { React } from './subjects/React';
+
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
+
+export const cTeacher: Subjects.Teacher = {
+  firstName: "John",
+  lastName: "Doe",
+  experienceTeachingC: 10,
+};
+
+// For Cpp subject
+console.log("C++:");
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// For Java subject
+console.log("\nJava:");
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// For React subject
+console.log("\nReact:");
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
